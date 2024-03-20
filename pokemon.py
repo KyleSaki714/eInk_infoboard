5da8587472f0c0dd90ef31fc694900d19af44b0b
KyleSaki714 <kyle13san@Gmail.com>
2024-03-20 16:49:23 -0700

    defined height and weight functions

--- pokemon.py
+++ pokemon.py
@@ -12,48 +12,91 @@ import cairo
 
 id = rand.randint(0, 1017)
 # id = 483
+# id = 506/
 # Favorites
 # 250
 
+def getFirstAppearance(pokeApi_data):
+
+    # if "game_indices" in pokeApi_data:
+    #     if len(pokeApi_data["game_indices"]) != 0:
+    #         # print("First Appearance: Pkmn " + pokeApi_data["game_indices"][0]["version"]["name"])
+    #         return pokeApi_data["game_indices"][0]["version"]["name"]
+    
+    text = ''
+    spritesVersions = pokeApi_data["sprites"]["versions"]
+    for gen in spritesVersions:
+        for game in spritesVersions[gen]:
+            if (spritesVersions[gen][game]["front_default"] != None):
+                # pokeInfo['firstAppearance'] += "first appearance: ", gen
+                text += gen
+                text = text.replace("eration-"," ",2)
+                text += ": "
+                # print("(games: ",end="")
+                for firstAppearanceGenGame in spritesVersions[gen]:
+                    # print(firstAppearanceGenGame,", ", end="")
+                    text += firstAppearanceGenGame
+                    text += " "
+                # print()
+                return text
+
+def getTypes(pokeApi_data):
+    text = ''
+    types = pokeApi_data["types"]
+    if (len(types) > 0):
+        text += types[0]["type"]["name"]
+        if (len(types) > 1):
+            for n in range(1, len(types)):
+                text += ", "
+                text += types[n]["type"]["name"]
+    # print(type["type"]["name"],", ",end="")
+        
+    return text
+
+# Converts the height from the given API Data
+# from decimeters to feet and inches. 
+def getHeight(pokeApi_data):
+    num = pokeApi_data["height"]
+    dmToFeet = 0.3280839895
+    return str(round(num * dmToFeet, 2)) + " ft"
+
+# Converts the weight from the given API data
+# from hectograms to pounds
+def getWeight(pokeApi_data):
+    num = pokeApi_data["weight"]
+    hgToPounds = 0.2204622622
+    return str(round(num * hgToPounds, 2)) + " lbs"
 
 BASE_URL_PKMN = "https://pokeapi.co/api/v2/pokemon/{0}"
 final_url_pkmn = BASE_URL_PKMN.format(id)
 
 BASE_URL_CHARACTERISTIC = ""
 
-pkmn_data = requests.get(final_url_pkmn).json()
+pokeApi_data = requests.get(final_url_pkmn).json()
 # pprint(pkmn_data)
 
-print(pkmn_data["sprites"]["front_default"])
-print(pkmn_data["name"])
-print(pkmn_data["id"])
-spritesVersions = pkmn_data["sprites"]["versions"]
-done = False
-for gen in spritesVersions:
-    if done == True: break
-    for game in spritesVersions[gen]:
-        if (spritesVersions[gen][game]["front_default"] != None):
-            print("first appearance: ",gen);
-            print("(games: ",end="")
-            for firstAppearanceGenGame in spritesVersions[gen]:
-                print(firstAppearanceGenGame,", ", end="")
-            done = True
-            print()
-            break
+pokeInfo = dict()
+pokeInfo["sprite"] = pokeApi_data["sprites"]["front_default"]
+pokeInfo['name'] = pokeApi_data["name"]
+pokeInfo['id'] = pokeApi_data["id"]
+pokeInfo['firstAppearance'] = getFirstAppearance(pokeApi_data);
+pokeInfo['height'] = getHeight(pokeApi_data)
+pokeInfo['weight'] = getWeight(pokeApi_data)
+pokeInfo['types'] = getTypes(pokeApi_data)
 
-# print("Height " + str(pkmn_data["height"]) + "dm")
-# print("Weight " + str(pkmn_data["weight"]) + "hg")
-types = pkmn_data["types"]
-for type in types:
-    print(type["type"]["name"],", ",end="")
-print()
-if "game_indices" in pkmn_data:
-    if len(pkmn_data["game_indices"]) != 0:
-        print("First Appearance: Pkmn " + pkmn_data["game_indices"][0]["version"]["name"])
+print(pokeInfo["sprite"])
+print(pokeInfo['name'])
+print(pokeApi_data['id'])
+print(pokeInfo['firstAppearance'])
+# print("Height " + str(pokeApi_data["height"]) + "dm")
+print(pokeInfo["height"])
+# print("Weight " + str(pokeApi_data["weight"]) + "hg")
+print(pokeInfo['weight'])
+print(pokeInfo['types'])
 
 # sprite = requests.get(pkmn_data["sprites"]["front_default"])
 # print(pkmn_data["sprites"]["front_default"])
-urllib.request.urlretrieve(pkmn_data["sprites"]["front_default"], "sprite.png")
+urllib.request.urlretrieve(pokeInfo['sprite'], "sprite.png")
 im = Image.open("sprite.png")
 im.size  # (364, 471)
 im.getbbox()  # (64, 89, 278, 267)
