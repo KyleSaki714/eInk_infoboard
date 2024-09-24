cc057fddada6131ff9b40f2df4910269939f5a1c
kyles13 <kyles13@cs.washington.edu>
2024-09-24 16:57:03 -0700

    refactor code for flask server

--- pokemon.py
+++ pokemon.py
@@ -9,8 +9,6 @@ from pprint import pprint
 import io
 import json
 
-id = rand.randint(0, 1017)
-
 def getFirstAppearance(pokeApi_data):
 
     # if "game_indices" in pokeApi_data:
@@ -62,62 +60,66 @@ def getWeight(pokeApi_data):
     hgToPounds = 0.2204622622
     return str(round(num * hgToPounds, 2)) + " lbs"
 
+# retrieves sprite image from pokeapi data,
+# then converts into 1-bit displayable on ssd1306 displays .
+# returns a string xbm image representation.
+def getSprite(pokeApi_data):
+    # get sprite from api
+    urllib.request.urlretrieve(pokeApi_data["sprites"]["front_default"], "sprite.png")
+    im = Image.open("sprite.png")
+
+    # crop to bounding box
+    im2 = im.crop(im.getbbox()) 
+
+    # resize sprite using thumbnail function size to 48x48 px, and use Bilinear resampling https://pillow.readthedocs.io/en/stable/reference/Image.html#resampling-filters
+    im2 = im2.resize((64, 64), 2)
+    im2.save("sprite_resized_unedited.png")
+
+    # convert image to 1 bit (b/w)
+    im2 = im2.convert('1')
+
+    # export to png for debugging purposes
+    try:
+        im2.save("sprite.png")
+    except:
+        im2.save("sprite.bmp", "BMP")
+
+    # export to xbm for esp32
+    im2.save("sprite.xbm", "XBM")
+
+    xbm = open("sprite.xbm", "r")
+    xbmTxt = xbm.read()
+    res = xbmTxt
+    xbm.close()
+    return res
+
+def generateNewPkmn():
+    # new random id
+    id = rand.randint(0, 1017)
+    
+    BASE_URL_PKMN = "https://pokeapi.co/api/v2/pokemon/{0}"
+    final_url_pkmn = BASE_URL_PKMN.format(id)
+
+    pokeApi_data = requests.get(final_url_pkmn).json()
+
+    pokeInfo = dict()
+    pokeInfo['name'] = pokeApi_data["name"]
+    pokeInfo['id'] = pokeApi_data["id"]
+    pokeInfo['firstAppearance'] = getFirstAppearance(pokeApi_data);
+    pokeInfo['height'] = getHeight(pokeApi_data)
+    pokeInfo['weight'] = getWeight(pokeApi_data)
+    pokeInfo['types'] = getTypes(pokeApi_data)
+    pokeInfo["sprite"] = getSprite(pokeApi_data)
+    return pokeInfo
+
 app = Flask(__name__)
 
-BASE_URL_PKMN = "https://pokeapi.co/api/v2/pokemon/{0}"
-final_url_pkmn = BASE_URL_PKMN.format(id)
-
-pokeApi_data = requests.get(final_url_pkmn).json()
-
-pokeInfo = dict()
-pokeInfo['name'] = pokeApi_data["name"]
-pokeInfo['id'] = pokeApi_data["id"]
-pokeInfo['firstAppearance'] = getFirstAppearance(pokeApi_data);
-pokeInfo['height'] = getHeight(pokeApi_data)
-pokeInfo['weight'] = getWeight(pokeApi_data)
-pokeInfo['types'] = getTypes(pokeApi_data)
-
-print(pokeInfo['name'])
-print(pokeApi_data['id'])
-print(pokeInfo['firstAppearance'])
-print(pokeInfo["height"])
-print(pokeInfo['weight'])
-print(pokeInfo['types'])
-
-# get sprite from api
-urllib.request.urlretrieve(pokeApi_data["sprites"]["front_default"], "sprite.png")
-im = Image.open("sprite.png")
-
-# crop to bounding box
-im2 = im.crop(im.getbbox()) 
-
-# resize sprite using thumbnail function size to 48x48 px, and use Bilinear resampling https://pillow.readthedocs.io/en/stable/reference/Image.html#resampling-filters
-im2 = im2.resize((64, 64), 2)
-im2.save("sprite_resized_unedited.png")
-
-# convert image to 1 bit (b/w)
-im2 = im2.convert('1')
-
-# export to png for debugging purposes
-try:
-    im2.save("sprite.png")
-except:
-    im2.save("sprite.bmp", "BMP")
-
-# export to xbm for esp32
-im2.save("sprite.xbm", "XBM")
-
-xbm = open("sprite.xbm", "r")
-xbmTxt = xbm.read()
-pokeInfo["sprite"] = xbmTxt
-xbm.close()
-
-print(pokeInfo)
-with open("pokeinfo.json" , "w") as write:
-    json.dump(pokeInfo, write)
+# with open("pokeinfo.json" , "w") as write:
+#     json.dump(pokeInfo, write)
 
 @app.get('/')
 def index():
+    pokeInfo = generateNewPkmn()
     return jsonify(pokeInfo)
 
 if __name__ == '__main__':
